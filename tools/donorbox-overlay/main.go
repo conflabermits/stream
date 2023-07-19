95088907ea4893d67ccdc2451f0badc2ab541726
conflabermits <christopher.dunaj@gmail.com>
2023-07-19 16:31:08 -0400

    Removed some unneceessary content, keep it simple

--- tools/donorbox-overlay/main.go
+++ tools/donorbox-overlay/main.go
@@ -4,46 +4,28 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
-	"time"
 )
 
 func main() {
-	// Start a goroutine to reload the contents every 30 seconds
-	contentReloadTicker := time.NewTicker(30 * time.Second)
-	quit := make(chan struct{})
-	go reloadContents(contentReloadTicker, quit)
-
-	// Start the web server
 	http.HandleFunc("/", serveHTML)
 	fmt.Println("Server listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func reloadContents(ticker *time.Ticker, quit chan struct{}) {
-	for {
-		select {
-		case <-ticker.C:
-			fmt.Println("Reloading contents...")
-			// Here you can update the contents or fetch new data from external sources.
-			// For this example, let's just print a message.
-			fmt.Println("Contents reloaded!")
-			// Here's some additional content, trying to be unique.
-			fmt.Println("Random integer: ", rand.Int())
-		case <-quit:
-			ticker.Stop()
-			return
-		}
-	}
-}
-
 func serveHTML(w http.ResponseWriter, r *http.Request) {
-	// Replace this with your actual HTML content if needed.
 	htmlContent := `
 		<!DOCTYPE html>
 		<html>
 		<head>
 			<meta charset="UTF-8">
 			<title>Auto-Reloading Web Page</title>
+            <style type="text/css">
+                .body { width: auto; }
+                div.main {
+                    font-size: 64px;
+                    color: yellowgreen;
+                }
+            </style>
 			<script>
 				function reloadPage() {
 					location.reload();
@@ -53,8 +35,9 @@ func serveHTML(w http.ResponseWriter, r *http.Request) {
 		</head>
 		<body>
 			<h1>Hello, this is an auto-reloading web page!</h1>
-            <h2>Here's some additional content, trying to be unique.</h2>
-            <p>Random integer: ` + fmt.Sprint(rand.Int()) + `</p>
+            <div class="main">
+                <p>Random integer: ` + fmt.Sprint(rand.Int()) + `</p>
+            </div>
 		</body>
 		</html>
 	`
